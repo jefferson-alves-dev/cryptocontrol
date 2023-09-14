c70bb1cc162a3e9664ba6b078b448a62662db5a8
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-14 12:26:19 -0300

    feat: add logic for create a new user on signup route

--- src/api/main/routes/auth.routes.ts
+++ src/api/main/routes/auth.routes.ts
@@ -1,6 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-var */
+/* eslint-disable prefer-const */
 import { loginValidationSchema, signupValidationSchema } from '@main/valitadions/schemas/auth'
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv'
 import { Request, Response, Router } from 'express'
 import Joi from 'joi'
+import { MongoClient, ObjectId } from 'mongodb'
+dotenv.config()
 
 const route = Router()
 
@@ -9,10 +16,77 @@ route.post('/signup', async (req: Request, res: Response) => {
 
   try {
     await signupValidationSchema.validateAsync({ name, email, password, passwordConfirmation })
-    return res.status(201).json({ message: 'The user was created successfully' })
   } catch (e: unknown) {
     const error = e as Joi.ValidationError
     return res.status(400).json({ message: 'Validation error', error: error.details[0]?.message })
+  }
+
+  const uri = process.env.DB_URI
+  if (!uri) {
+    console.log('DB_URI is not set')
+    return res.status(500).json({ message: 'Internal server error' })
+  }
+  const mongo = new MongoClient(uri)
+
+  try {
+    const client = await mongo.connect()
+    const collection = client.db().collection('users')
+    const user = await collection.findOne({ email })
+    if (user) {
+      return res.status(400).json({ message: 'User already exists' })
+    }
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Internal server error' })
+  } finally {
+    await mongo.close()
+    console.log('MongoDB connection closed')
+  }
+
+  if (!process.env.SALT_HASH) {
+    console.log('SALT_HASH is not set')
+    return res.status(500).json({ message: 'Internal server error' })
+  }
+
+  const hashedPassword = await bcrypt.hash(password, Number(process.env.SALT_HASH))
+
+  try {
+    const client = await mongo.connect()
+    const collection = client.db().collection('users')
+    const result = await collection.insertOne({
+      name,
+      email,
+      password: hashedPassword,
+      createdAt: new Date().getTime(),
+      isActive: true,
+    })
+    const id = result.insertedId.toString()
+    const userData = await collection.findOne({ _id: new ObjectId(id) })
+
+    if (!userData) {
+      return res.status(500).json({ message: 'Internal server error' })
+    }
+    const { _id, password, createdAt, isActive, ...rest } = userData
+    const user = {
+      id: _id.toHexString(),
+      ...rest,
+      createdAt: new Date(createdAt).toLocaleString('pt-BR', {
+        year: 'numeric',
+        month: '2-digit',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+      }),
+    }
+
+    return res.status(201).json({ message: `The user with id = ${id} has been created`, user })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Internal server error. The user could not be created.' })
+  } finally {
+    await mongo.close()
+    console.log('MongoDB connection closed')
   }
 })
 
