b9eeb037ec574846a959b7ade304f4181a1ace16
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-20 10:25:36 -0300

    feat: add hasher param to IUserUsecase create method

--- src/api/domain/usecases/user/index.ts
+++ src/api/domain/usecases/user/index.ts
@@ -1,9 +1,11 @@
 import { TUser } from '@domain/types'
 
+import { IHasher } from '../cryptography'
+
 export interface IUserUsecase {
   getByEmail(email: string): Promise<TUserUsecase.Result>
   getById(id: string): Promise<TUserUsecase.Result>
-  create(userData: TUser.Create): Promise<TUser.Result>
+  create(userData: TUser.Create, hasher: IHasher): Promise<TUser.Result>
 }
 
 export namespace TUserUsecase {
