fd49f11c36710286de0108cc3365f2cd83753e2b
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-21 11:04:23 -0300

    feat(factories): update 'makeGetAllWalletsController' to include UserRepository

--- src/api/bootstrap/factories/controllers/wallets/get-all-wallets-controller-factory.ts
+++ src/api/bootstrap/factories/controllers/wallets/get-all-wallets-controller-factory.ts
@@ -1,10 +1,11 @@
-import { WalletRepositoryMongoDB } from '@infra/database/mongodb/repositories'
+import { UserRepositoryMongoDB, WalletRepositoryMongoDB } from '@infra/database/mongodb/repositories'
 import { GetAllWalletsControler } from '@presentation/controllers/wallets'
 import { IController } from '@presentation/protocols/contracts'
 import { WalletService } from '@services/usecases/WalletService'
 
 export const makeGetAllWalletsController = (): IController => {
   const walletRepository = new WalletRepositoryMongoDB()
-  const walletService = new WalletService(walletRepository)
+  const userRepository = new UserRepositoryMongoDB()
+  const walletService = new WalletService(walletRepository, userRepository)
   return new GetAllWalletsControler(walletService)
 }
