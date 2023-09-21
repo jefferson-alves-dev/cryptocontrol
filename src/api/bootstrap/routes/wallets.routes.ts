f58edb4b8e27ca5c7f45b90f472c7fb2cf7e2314
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-20 21:06:26 -0300

    feat: implement correct adaptations on wallet routes (post /, get /:walletID, get /)

--- src/api/bootstrap/routes/wallets.routes.ts
+++ src/api/bootstrap/routes/wallets.routes.ts
@@ -1,26 +1,18 @@
+import { RouteExpressAdapter } from '@bootstrap/adapters'
+import {
+  makeCreateWalletController,
+  makeGetAllWalletsController,
+  makeGetWalletController,
+} from '@bootstrap/factories/controllers/wallets'
 import { Request, Response, Router } from 'express'
 
 const route = Router()
 
-route.get('/:id', async (req: Request, res: Response) => {
-  const { id } = req.params
-  if (!id) {
-    return res.status(400).json({ message: 'Wallet id is required' })
-  }
-  return res.status(201).json({ message: id, verbal: 'get' })
-})
+route.get('/:walletID', new RouteExpressAdapter(makeGetWalletController()).adapt)
 
-route.post('/', async (req: Request, res: Response) => {
-  const { name } = req.body
-  if (!name) {
-    return res.status(400).json({ message: 'Wallet name is required' })
-  }
-  return res.status(201).json({ message: name, verbal: 'post' })
-})
+route.post('/', new RouteExpressAdapter(makeCreateWalletController()).adapt)
 
-route.get('/', async (req: Request, res: Response) => {
-  return res.status(201).json({ message: ['wallet 1', 'wallet 2', 'wallet 3'], verbal: 'getAll' })
-})
+route.get('/', new RouteExpressAdapter(makeGetAllWalletsController()).adapt)
 
 route.put('/:id', async (req: Request, res: Response) => {
   const { id } = req.params
