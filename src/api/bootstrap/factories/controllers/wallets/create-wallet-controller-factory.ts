0321a0211d261822e4cd1da01b0e8550b8c8f3b1
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-21 11:03:10 -0300

    feat(factories): update 'makeCreateWalletController' to include UserRepository

--- src/api/bootstrap/factories/controllers/wallets/create-wallet-controller-factory.ts
+++ src/api/bootstrap/factories/controllers/wallets/create-wallet-controller-factory.ts
@@ -1,4 +1,4 @@
-import { WalletRepositoryMongoDB } from '@infra/database/mongodb/repositories/'
+import { UserRepositoryMongoDB, WalletRepositoryMongoDB } from '@infra/database/mongodb/repositories/'
 import { JoiValidatorAdapter } from '@presentation/adapters'
 import { CreateWalletController } from '@presentation/controllers/wallets/'
 import { IController } from '@presentation/protocols/contracts'
@@ -7,7 +7,8 @@ import { WalletService } from '@services/usecases/WalletService'
 
 export const makeCreateWalletController = (): IController => {
   const walletRepository = new WalletRepositoryMongoDB()
-  const walletService = new WalletService(walletRepository)
+  const userRepository = new UserRepositoryMongoDB()
+  const walletService = new WalletService(walletRepository, userRepository)
   const validator = new JoiValidatorAdapter(createWalletSchema)
   return new CreateWalletController(validator, walletService)
 }
