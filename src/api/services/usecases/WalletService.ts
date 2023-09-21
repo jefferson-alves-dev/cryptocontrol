3453870e219c889e8db5cc82c5282838a473dc9b
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-20 21:00:22 -0300

    refactor: implement WalletService features

--- src/api/services/usecases/WalletService.ts
+++ src/api/services/usecases/WalletService.ts
@@ -1,15 +1,42 @@
 import { TWallet } from '@domain/types'
 import { IWalletUsecase } from '@domain/usecases/wallet'
+import { IWalletRepository } from '@services/protocols/contracts/database/repositories'
 
 export class WalletService implements IWalletUsecase {
   constructor(private readonly walletRepository: IWalletRepository) {}
-  async create(walletData: TWallet.Create): Promise<TWallet.Created> {}
+  async create(walletData: TWallet.Create): Promise<TWallet.Created> {
+    const wallet = await this.walletRepository.create({
+      userID: walletData.userID,
+      name: walletData.name,
+      isActive: true,
+      createdAt: new Date().getTime(),
+      updatedAt: null,
+      desactivatedAt: null,
+    })
 
-  async deleteById(id: string): void {}
+    return {
+      error: null,
+      data: { id: wallet.id },
+    }
+  }
 
-  async getAll(): Promise<TWallet.Full> {}
+  async getById(walletID: string, userID: string): Promise<TWallet.Full | null> {
+    const wallet = await this.walletRepository.getById(walletID, userID)
+    return wallet || null
+  }
 
-  async getById(id: string): Promise<TWallet.Full> {}
+  async getAll(userID: string): Promise<TWallet.Full[] | null> {
+    const wallets = await this.walletRepository.getAll(userID)
+    return wallets || null
+  }
 
-  async updateById(id: string, walletData: TWallet.Create): void {}
+  async deleteById(walletID: string, userID: string): Promise<void> {
+    await this.walletRepository.deleteById(walletID, userID)
+    return
+  }
+
+  async updateById(walletID: string, walletData: TWallet.Update): Promise<void> {
+    await this.walletRepository.updateById(walletID, walletData)
+    return
+  }
 }
