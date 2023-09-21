b805d7271f62c6556fee112478447625ce7ba7c7
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-21 09:52:22 -0300

    feat: add unauthorized helper method on presentation layer

--- src/api/presentation/helpers/http-responses.ts
+++ src/api/presentation/helpers/http-responses.ts
@@ -23,3 +23,8 @@ export const success = <T>(data: T): HttpResponse => ({
   statusCode: 200,
   body: data,
 })
+
+export const unauthorized = (error: Error): HttpResponse => ({
+  statusCode: 401,
+  body: error.message,
+})
