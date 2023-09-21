5710f96d6d5bce7af4116ce35a58618a3228cb1f
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-21 10:55:15 -0300

    feat(IUserRepository): add isUserActive method to interface

--- src/api/services/protocols/contracts/database/repositories/user-repository.ts
+++ src/api/services/protocols/contracts/database/repositories/user-repository.ts
@@ -5,4 +5,5 @@ export interface IUserRepository {
   getByEmail(email: string): Promise<TUser.Full | null>
   getById(id: string): Promise<TUser.Full | null>
   create(data: TUserRepositoryData.Create): Promise<TUserRepositoryData.Created>
+  isUserActive(userID: string): Promise<boolean>
 }
