02cf6e1e1aa43440c29022a3aa01a3584ef66f24
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-22 06:13:24 -0300

    refactor(IUserRepository): replace TUser.Full with TUserRepositoryData.UserInfos

--- src/api/services/protocols/contracts/database/repositories/user-repository.ts
+++ src/api/services/protocols/contracts/database/repositories/user-repository.ts
@@ -1,9 +1,8 @@
-import { TUser } from '@domain/types'
 import { TUserRepositoryData } from '@services/protocols/types'
 
 export interface IUserRepository {
-  getByEmail(email: string): Promise<TUser.Full | null>
-  getById(id: string): Promise<TUser.Full | null>
+  getByEmail(email: string): Promise<TUserRepositoryData.UserInfos | null>
+  getById(id: string): Promise<TUserRepositoryData.UserInfos | null>
   create(data: TUserRepositoryData.Create): Promise<TUserRepositoryData.Created>
   isUserActive(userID: string): Promise<boolean>
 }
