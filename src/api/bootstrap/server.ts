1a1385e80d146a5096563d089805ec4bae45fb7a
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-22 06:16:45 -0300

    feat(server): integrate MongoDB setup and graceful shutdown

--- src/api/bootstrap/server.ts
+++ src/api/bootstrap/server.ts
@@ -1,6 +1,8 @@
 import 'module-alias/register'
 
 import routes from '@bootstrap/routes/index.routes'
+import { MongoDBClientSingleton } from '@infra/database/clients'
+import { MongoHelper } from '@infra/database/mongodb/helpers/MongoHelper'
 import express from 'express'
 
 import { validateJsonFormat } from './middlewares'
@@ -13,4 +15,22 @@ server.use(validateJsonFormat)
 
 server.use(routes)
 
-server.listen(3000, () => console.log('Running on port 3000'))
+MongoDBClientSingleton.getInstance()
+  .then(new MongoHelper().createIndexes)
+  .then(() => {
+    server.listen(3000, () => console.log('Running on port 3000'))
+  })
+
+process.on('SIGINT', gracefulShutdown)
+process.on('SIGTERM', gracefulShutdown)
+
+async function gracefulShutdown() {
+  console.log('\nShutting down gracefully...')
+  try {
+    await MongoDBClientSingleton.disconnect()
+    process.exit(0)
+  } catch (error) {
+    console.error('Error during graceful shutdown:', error)
+    process.exit(1)
+  }
+}
