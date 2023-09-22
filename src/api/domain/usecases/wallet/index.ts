8501ce703821bbef9bfc5df376e1043530d0b99d
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-22 06:09:22 -0300

    refactor(IWalletUsecase): change return type of deleteById to TWallet.Deleted

--- src/api/domain/usecases/wallet/index.ts
+++ src/api/domain/usecases/wallet/index.ts
@@ -5,5 +5,5 @@ export interface IWalletUsecase {
   getAll(userID: string): Promise<TWallet.Full[] | null>
   create(walletData: TWallet.Create): Promise<TWallet.Created>
   updateById(walletID: string, walletData: TWallet.Update): Promise<void>
-  deleteById(walletID: string, userID: string): Promise<void>
+  deleteById(walletID: string, userID: string): Promise<TWallet.Deleted>
 }
