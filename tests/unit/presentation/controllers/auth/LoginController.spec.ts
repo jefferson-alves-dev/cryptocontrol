31430513e245d7939da71706224a77f414f50689
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-26 20:15:01 -0300

    test: ensure LoginController returns correct http response if userService throws

--- tests/unit/presentation/controllers/auth/LoginController.spec.ts
+++ tests/unit/presentation/controllers/auth/LoginController.spec.ts
@@ -123,5 +123,16 @@ describe('LoginController', () => {
         body: new Error('Internal server error'),
       })
     })
+
+    it('should return correct http response if userService throws', async () => {
+      const { sut, userServiceSpy } = makeSut()
+      jest.spyOn(userServiceSpy, 'getByEmail').mockImplementationOnce(throwError)
+      const httpRequest = makeFakeRequest()
+      const result = await sut.handle(httpRequest)
+      expect(result).toEqual({
+        statusCode: 500,
+        body: new Error('Internal server error'),
+      })
+    })
   })
 })
