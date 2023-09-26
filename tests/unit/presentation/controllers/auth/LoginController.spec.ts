f48010fe2d668dba94353e77522e347db340f087
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-26 20:15:38 -0300

    test: ensure LoginController returns correct http response if hasher throws

--- tests/unit/presentation/controllers/auth/LoginController.spec.ts
+++ tests/unit/presentation/controllers/auth/LoginController.spec.ts
@@ -134,5 +134,16 @@ describe('LoginController', () => {
         body: new Error('Internal server error'),
       })
     })
+
+    it('should return correct http response if hasher throws', async () => {
+      const { sut, hasherSpy } = makeSut()
+      jest.spyOn(hasherSpy, 'compare').mockImplementationOnce(throwError)
+      const httpRequest = makeFakeRequest()
+      const result = await sut.handle(httpRequest)
+      expect(result).toEqual({
+        statusCode: 500,
+        body: new Error('Internal server error'),
+      })
+    })
   })
 })
