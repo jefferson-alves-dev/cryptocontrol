f6403882242b2a7ecd733890783f2c47fea03115
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-26 11:03:00 -0300

    test: ensure JoiValidatorAdapter returns correct result when max length of string is exceeded

--- tests/unit/presentation/adapers/JoiValidatorAdapter.spec.ts
+++ tests/unit/presentation/adapers/JoiValidatorAdapter.spec.ts
@@ -1,6 +1,11 @@
 import { faker } from '@faker-js/faker'
 import { JoiValidatorAdapter } from '@presentation/adapters'
-import { InvalidEmailError, InvalidStringFormatError, MissingParameterError } from '@presentation/errors'
+import {
+  InvalidEmailError,
+  InvalidStringFormatError,
+  MissingParameterError,
+  StringLengthExceededError,
+} from '@presentation/errors'
 import Joi from 'joi'
 
 describe('JoiValidator Adapter', () => {
@@ -44,6 +49,21 @@ describe('JoiValidator Adapter', () => {
     })
     expect(result).toEqual({
       error: new InvalidEmailError('email'),
+      value: undefined,
+    })
+  })
+
+  it('should return correct result when a expected max length of string is exceeded', async () => {
+    const schema = Joi.object({
+      name: Joi.string().max(10).required(),
+    })
+    const sut = new JoiValidatorAdapter(schema)
+    const string = faker.string.alphanumeric(11)
+    const result = await sut.validate({
+      name: string,
+    })
+    expect(result).toEqual({
+      error: new StringLengthExceededError(string, 10),
       value: undefined,
     })
   })
