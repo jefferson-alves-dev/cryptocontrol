9dc15720ad7c5e2eb66d0197f6becf2f5899ef93
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-27 19:58:21 -0300

    refactor(controller): add error handling to GetAllWalletsController

--- src/api/presentation/controllers/wallets/GetAllWalletsController.ts
+++ src/api/presentation/controllers/wallets/GetAllWalletsController.ts
@@ -1,15 +1,19 @@
 import { IWalletUsecase } from '@domain/usecases/wallet'
 import { MissingParameterError } from '@presentation/errors'
-import { badRequest, success } from '@presentation/helpers'
+import { badRequest, serverError, success } from '@presentation/helpers'
 import { IController } from '@presentation/protocols/contracts'
 import { HttpResponse } from '@presentation/protocols/types'
 
 export class GetAllWalletsControler implements IController {
   constructor(private readonly walletService: IWalletUsecase) {}
   async handle(httpRequest: GetAllWalletsControler.Request): Promise<HttpResponse> {
-    if (!httpRequest?.userData?.userID) return badRequest(new MissingParameterError('userID'))
-    const wallet = await this.walletService.getAll(httpRequest.userData.userID)
-    return success(wallet)
+    try {
+      if (!httpRequest?.userData?.userID) return badRequest(new MissingParameterError('userID'))
+      const wallet = await this.walletService.getAll(httpRequest.userData.userID)
+      return success(wallet)
+    } catch (error) {
+      return serverError()
+    }
   }
 }
 
