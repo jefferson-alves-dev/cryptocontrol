29825a2d0fb9e8428e57a644594d2156b385e29c
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-27 19:39:22 -0300

    test: ensure DeleteWalletController returns correct http response if validator throws

--- tests/unit/presentation/controllers/wallets/DeleteWalletController.spec.ts
+++ tests/unit/presentation/controllers/wallets/DeleteWalletController.spec.ts
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker'
 import { DeleteWalletController } from '@presentation/controllers/wallets'
+import { throwError } from '@tests/helpers'
 
 import { ValidatorSpy, WalletServiceSpy } from '../../mocks'
 
@@ -63,6 +64,19 @@ describe('DeleteWalletController', () => {
       expect(httpResponse).toEqual({
         statusCode: 404,
         body: new Error(),
+      })
+    })
+  })
+
+  describe('throws', () => {
+    it('should return correct http response if validator throws', async () => {
+      const { sut, validatorSpy } = makeSut()
+      jest.spyOn(validatorSpy, 'validate').mockImplementationOnce(throwError)
+      const httpRequest = makeFakeRequest()
+      const result = await sut.handle(httpRequest)
+      expect(result).toEqual({
+        statusCode: 500,
+        body: new Error('Internal server error'),
       })
     })
   })
