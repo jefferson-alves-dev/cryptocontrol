097fbfa3903c609daac3d0b5eb6af4de312c0acd
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-09-26 21:19:38 -0300

    refactor(CreateWalletController): add error handling and adjust success response

--- src/api/presentation/controllers/wallets/CreateWalletController.ts
+++ src/api/presentation/controllers/wallets/CreateWalletController.ts
@@ -1,6 +1,6 @@
 import { IWalletUsecase } from '@domain/usecases/wallet'
 import { IValidator } from '@presentation/adapters/protocols/contracts'
-import { badRequest, success } from '@presentation/helpers'
+import { badRequest, created, serverError } from '@presentation/helpers'
 import { IController } from '@presentation/protocols/contracts'
 import { HttpResponse } from '@presentation/protocols/types'
 
@@ -10,21 +10,25 @@ export class CreateWalletController implements IController {
     private readonly walletService: IWalletUsecase,
   ) {}
   async handle(httpRequest: CreateWalletController.Request): Promise<HttpResponse> {
-    const validate = await this.validator.validate(httpRequest.body)
-    if (validate.error) {
-      return badRequest(validate.error)
+    try {
+      const validate = await this.validator.validate(httpRequest.body)
+      if (validate.error) {
+        return badRequest(validate.error)
+      }
+
+      const createWallet = await this.walletService.create({
+        name: httpRequest.body.name,
+        userID: httpRequest.userData.userID,
+      })
+
+      if (createWallet.error) {
+        return badRequest(createWallet.error)
+      }
+
+      return created()
+    } catch (error) {
+      return serverError()
     }
-
-    const createWallet = await this.walletService.create({
-      name: httpRequest.body.name,
-      userID: httpRequest.userData.userID,
-    })
-
-    if (createWallet.error) {
-      return badRequest(createWallet.error)
-    }
-
-    return success(createWallet.data)
   }
 }
 
