982ed21cd6bb96d86eaaa50412cf2f6561725130
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-10-03 10:30:44 -0300

    feat: add getById() to IContributionRepository interface

--- src/api/services/protocols/contracts/database/repositories/contribution-repository.ts
+++ src/api/services/protocols/contracts/database/repositories/contribution-repository.ts
@@ -2,4 +2,5 @@ import { TContributionRepository } from '@services/protocols/types/contribution-
 
 export interface IContributionRepository {
   create(contributionData: TContributionRepository.Create): Promise<TContributionRepository.CreateResult>
+  getById(contributionID: string, userID: string): Promise<TContributionRepository.GetResult>
 }
