2b981716f28f66a5d8d0d9b804edc52f8d037c87
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-10-09 19:46:58 -0300

    feat: add CreateContributionController

---
+++ src/api/presentation/controllers/contributions/CreateContributionController.ts
@@ -0,0 +1,40 @@
+import { TContribution } from '@domain/types'
+import { IContributionUsecase } from '@domain/usecases/contribution'
+import { IValidator } from '@presentation/adapters/protocols/contracts'
+import { badRequest, created, serverError } from '@presentation/helpers'
+import { IController } from '@presentation/protocols/contracts'
+import { HttpResponse } from '@presentation/protocols/types'
+
+export class CreateContributionController implements IController {
+  constructor(
+    private readonly validator: IValidator,
+    private readonly contributionService: IContributionUsecase,
+  ) {}
+  async handle(httpRequest: CreateContributionController.Request): Promise<HttpResponse> {
+    try {
+      const validate = await this.validator.validate(httpRequest.body)
+      if (validate.error) {
+        return badRequest(validate.error)
+      }
+
+      const createContribution = await this.contributionService.create(httpRequest.body)
+
+      if (createContribution.error) {
+        return badRequest(createContribution.error)
+      }
+
+      return created()
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
+
+export namespace CreateContributionController {
+  export type Request = {
+    body: TContribution.Create
+    userData: {
+      [key: string]: string
+    }
+  }
+}
