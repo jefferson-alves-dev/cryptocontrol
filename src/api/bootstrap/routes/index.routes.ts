17d24e826065381c19372d70816c8a40e790fe29
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-10-09 19:50:09 -0300

    refactor(routes): rename 'deposits' routes to 'contributions'

--- src/api/bootstrap/routes/index.routes.ts
+++ src/api/bootstrap/routes/index.routes.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from '@bootstrap/middlewares'
 import { Router } from 'express'
 
 import authRoutes from './auth.routes'
-import depositsRoutes from './deposits.routes'
+import contributionsRoutes from './contributions.routes'
 import usersRoutes from './users.routes'
 import walletsRoutes from './wallets.routes'
 import withdrawalsRoutes from './withdrawals.routes'
@@ -10,7 +10,7 @@ import withdrawalsRoutes from './withdrawals.routes'
 const router = Router()
 
 router.use('/auth', authRoutes)
-router.use('/deposits', depositsRoutes)
+router.use('/contributions', contributionsRoutes)
 router.use('/users', usersRoutes)
 router.use('/wallets', authMiddleware, walletsRoutes)
 router.use('/withdrawals', withdrawalsRoutes)
