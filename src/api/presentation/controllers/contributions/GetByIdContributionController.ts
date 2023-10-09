252e1b013a3a50a9cc0de72ce358ffb578c6ac00
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-10-09 19:47:53 -0300

    feat: add GetByIdContributionController

---
+++ src/api/presentation/controllers/contributions/GetByIdContributionController.ts
@@ -0,0 +1,42 @@
+import { IContributionUsecase } from '@domain/usecases/contribution'
+import { IValidator } from '@presentation/adapters/protocols/contracts'
+import { badRequest, serverError, success } from '@presentation/helpers'
+import { IController } from '@presentation/protocols/contracts'
+import { HttpResponse } from '@presentation/protocols/types'
+
+export class GetByIdContributionController implements IController {
+  constructor(
+    private readonly validator: IValidator,
+    private readonly contributionService: IContributionUsecase,
+  ) {}
+  async handle(httpRequest: GetByIdContributionController.Request): Promise<HttpResponse> {
+    try {
+      const validate = await this.validator.validate(httpRequest.body)
+      if (validate.error) {
+        return badRequest(validate.error)
+      }
+
+      const contribution = await this.contributionService.getById(
+        httpRequest.body.contributionID,
+        httpRequest.userData.userID,
+      )
+
+      if (contribution.error) {
+        return badRequest(contribution.error)
+      }
+
+      return success(contribution)
+    } catch (error) {
+      return serverError()
+    }
+  }
+}
+
+export namespace GetByIdContributionController {
+  export type Request = {
+    body: { contributionID: string }
+    userData: {
+      [key: string]: string
+    }
+  }
+}
