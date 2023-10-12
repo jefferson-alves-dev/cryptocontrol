bd17a29b0cb1bbe1af801df57665d2318e8313b2
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-10-12 20:31:28 -0300

    refactor(contribution.routes): add RouteExppressAdapter and controllers factories to some routes

--- src/api/bootstrap/routes/contributions.routes.ts
+++ src/api/bootstrap/routes/contributions.routes.ts
@@ -1,26 +1,18 @@
+import { RouteExpressAdapter } from '@bootstrap/adapters'
+import {
+  makeCreateContributionController,
+  makeGetAllContributionController,
+  makeGetByIdContributionController,
+} from '@bootstrap/factories/controllers/contributions'
 import { Request, Response, Router } from 'express'
 
 const route = Router()
 
-route.get('/:id', async (req: Request, res: Response) => {
-  const { id } = req.params
-  if (!id) {
-    return res.status(400).json({ message: 'deposits id is required' })
-  }
-  return res.status(201).json({ message: id, verbal: 'get' })
-})
+route.get('/:contributionID', new RouteExpressAdapter(makeGetByIdContributionController()).adapt)
 
-route.post('/', async (req: Request, res: Response) => {
-  const { name } = req.body
-  if (!name) {
-    return res.status(400).json({ message: 'deposits name is required' })
-  }
-  return res.status(201).json({ message: name, verbal: 'post' })
-})
+route.post('/', new RouteExpressAdapter(makeCreateContributionController()).adapt)
 
-route.get('/', async (req: Request, res: Response) => {
-  return res.status(201).json({ message: ['deposits 1', 'deposits 2', 'deposits 3'], verbal: 'getAll' })
-})
+route.get('/', new RouteExpressAdapter(makeGetAllContributionController()).adapt)
 
 route.put('/:id', async (req: Request, res: Response) => {
   const { id } = req.params
