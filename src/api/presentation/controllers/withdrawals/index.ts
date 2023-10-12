0aa91d77f0cd581f48b16e126f7b3a3be4039bb1
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-10-12 20:44:31 -0300

    feat: add index to export all Withdrawal Controllers

---
+++ src/api/presentation/controllers/withdrawals/index.ts
@@ -0,0 +1,4 @@
+export * from './CreateWithdrawalController'
+export * from './DeleteWithdrawalController'
+export * from './GetAllWithdrawalController'
+export * from './GetByIdWithdrawalController'
