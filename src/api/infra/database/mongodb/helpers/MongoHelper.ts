cd1e43118d396c4140eea06d587db8b8ef763956
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-10-12 20:49:20 -0300

    feat: add createIndex to withdrawals, contributions, cryptos and fiatCurrecies Collections

--- src/api/infra/database/mongodb/helpers/MongoHelper.ts
+++ src/api/infra/database/mongodb/helpers/MongoHelper.ts
@@ -4,9 +4,17 @@ export class MongoHelper {
   async createIndexes() {
     const userCollection = await MongoDBClientSingleton.getCollection('users')
     const walletCollection = await MongoDBClientSingleton.getCollection('wallets')
+    const withdrawalCollection = await MongoDBClientSingleton.getCollection('withdrawals')
+    const contributionCollection = await MongoDBClientSingleton.getCollection('contributions')
+    const cryptosCollection = await MongoDBClientSingleton.getCollection('cryptos')
+    const fiatCurrenciesCollection = await MongoDBClientSingleton.getCollection('fiatCurrencies')
     await userCollection.createIndex({ email: 1, isActive: 1 })
     await userCollection.createIndex({ isActive: 1 })
     await walletCollection.createIndex({ _id: 1, userID: 1, isActive: 1 })
     await walletCollection.createIndex({ userID: 1, isActive: 1 })
+    await withdrawalCollection.createIndex({ _id: 1, symbol: 1, isActive: 1 })
+    await contributionCollection.createIndex({ _id: 1, walletID: 1, userID: 1, isActive: 1 })
+    await cryptosCollection.createIndex({ _id: 1, criptoID: 1, symbol: 1, isActive: 1 })
+    await fiatCurrenciesCollection.createIndex({ _id: 1, fiatID: 1, symbol: 1, isActive: 1 })
   }
 }
