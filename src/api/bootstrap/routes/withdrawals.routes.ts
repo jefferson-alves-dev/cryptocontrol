e06dd5e2b55748a075bf8db17b287280a56ee783
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-10-12 20:48:10 -0300

    refactor(withdrawals.routes): add RouteExpressAdapter and controllers factories

--- src/api/bootstrap/routes/withdrawals.routes.ts
+++ src/api/bootstrap/routes/withdrawals.routes.ts
@@ -1,28 +1,20 @@
+import { RouteExpressAdapter } from '@bootstrap/adapters'
+import {
+  makeCreateWithdrawalController,
+  makeGetAllWithdrawalController,
+  makeGetByIdWithdrawalController,
+} from '@bootstrap/factories/controllers/withdrawals'
 import { Request, Response, Router } from 'express'
 
 const route = Router()
 
-route.get('/:id', async (req: Request, res: Response) => {
-  const { id } = req.params
-  if (!id) {
-    return res.status(400).json({ message: 'withdrawals id is required' })
-  }
-  return res.status(201).json({ message: id, verbal: 'get' })
-})
+route.get('/:withdrawalID', new RouteExpressAdapter(makeGetByIdWithdrawalController()).adapt)
 
-route.post('/', async (req: Request, res: Response) => {
-  const { name } = req.body
-  if (!name) {
-    return res.status(400).json({ message: 'withdrawals name is required' })
-  }
-  return res.status(201).json({ message: name, verbal: 'post' })
-})
+route.post('/', new RouteExpressAdapter(makeCreateWithdrawalController()).adapt)
 
-route.get('/', async (req: Request, res: Response) => {
-  return res.status(201).json({ message: ['withdrawals 1', 'withdrawals 2', 'withdrawals 3'], verbal: 'getAll' })
-})
+route.get('/', new RouteExpressAdapter(makeGetAllWithdrawalController()).adapt)
 
-route.put('/:id', async (req: Request, res: Response) => {
+route.put('/:withdrawalID', async (req: Request, res: Response) => {
   const { id } = req.params
   if (!id) {
     return res.status(400).json({ message: 'withdrawals id is required' })
@@ -30,7 +22,7 @@ route.put('/:id', async (req: Request, res: Response) => {
   return res.status(201).json({ message: id, verbal: 'put' })
 })
 
-route.delete('/:id', async (req: Request, res: Response) => {
+route.delete('/:withdrawalID', async (req: Request, res: Response) => {
   const { id } = req.params
   if (!id) {
     return res.status(400).json({ message: 'withdrawals id is required' })
