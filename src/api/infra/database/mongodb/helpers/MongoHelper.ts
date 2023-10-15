f13d85da533834a8529f5f6491ecc36b76384182
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-10-15 07:30:19 -0300

    feat: add new indexs to contribution collection and withdrawal collection

--- src/api/infra/database/mongodb/helpers/MongoHelper.ts
+++ src/api/infra/database/mongodb/helpers/MongoHelper.ts
@@ -12,8 +12,10 @@ export class MongoHelper {
     await userCollection.createIndex({ isActive: 1 })
     await walletCollection.createIndex({ _id: 1, userID: 1, isActive: 1 })
     await walletCollection.createIndex({ userID: 1, isActive: 1 })
-    await withdrawalCollection.createIndex({ _id: 1, symbol: 1, isActive: 1 })
-    await contributionCollection.createIndex({ _id: 1, walletID: 1, userID: 1, isActive: 1 })
+    await contributionCollection.createIndex({ _id: 1, walletID: 1, symbol: 1, userID: 1, isActive: 1 })
+    await withdrawalCollection.createIndex({ _id: 1, walletID: 1, symbol: 1, userID: 1, isActive: 1 })
+    await contributionCollection.createIndex({ isActive: 1, userID: 1 })
+    await withdrawalCollection.createIndex({ isActive: 1, userID: 1 })
     await cryptosCollection.createIndex({ _id: 1, criptoID: 1, symbol: 1, isActive: 1 })
     await fiatCurrenciesCollection.createIndex({ _id: 1, fiatID: 1, symbol: 1, isActive: 1 })
   }
